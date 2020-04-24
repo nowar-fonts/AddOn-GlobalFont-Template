454ec686051d6cf476484304c04208bd497d7b72
Cyano Hao <c@cyano.cn>
2020-04-24 10:03:27 +0800

    update build script

--- build.bash
+++ build.bash
@@ -1,4 +1,8 @@
-interface=(11304 80300 90001)
+# Usage:
+#   bash build.bash <interface>
+#
+# e.g.
+#   bash build.bash 80300
 
 edition=(Std Pro)
 declare -A familySans=([Std]=WarSans [Pro]=NowarSans)
@@ -13,45 +17,42 @@ rm -r build/ out/
 mkdir -p out/7z
 mkdir -p out/zip
 
-for i in ${interface[@]}
+v=$(cat $1/\!GlobalFont_Template/\!GlobalFont_Template.toc | grep "## Version" | cut -d\  -f3)
+for e in ${edition[@]}
 do
-	v=$(cat $i/\!GlobalFont_Template/\!GlobalFont_Template.toc | grep "## Version" | cut -d\  -f3)
-	for e in ${edition[@]}
+	sans=${familySans[$e]}
+	ui=${familyUI[$e]}
+	for w in ${weight[@]}
 	do
-		sans=${familySans[$e]}
-		ui=${familyUI[$e]}
-		for w in ${weight[@]}
-		do
-			normal=${normalStyle[$w]}
-			cond=${condStyle[$w]}
-			ext=${extStyle[$w]}
+		normal=${normalStyle[$w]}
+		cond=${condStyle[$w]}
+		ext=${extStyle[$w]}
 
-			mkdir -p build/$i/\!GlobalFont_$e$w
-			cp $i/\!GlobalFont_Template/\!GlobalFont_Template.toc build/$i/\!GlobalFont_$e$w/\!GlobalFont_$e$w.toc
-			sed -i "s/__REPLACE_IN_BUILD__ADDON_NAME__/Global Font ($e $w)/" build/$i/\!GlobalFont_$e$w/\!GlobalFont_$e$w.toc
+		mkdir -p build/$1/\!GlobalFont_$e$w
+		cp $1/\!GlobalFont_Template/\!GlobalFont_Template.toc build/$1/\!GlobalFont_$e$w/\!GlobalFont_$e$w.toc
+		sed -i "s/__REPLACE_IN_BUILD__ADDON_NAME__/Global Font ($e $w)/" build/$1/\!GlobalFont_$e$w/\!GlobalFont_$e$w.toc
 
-			cp $i/\!GlobalFont_Template/{Core.lua,FixedSize.xml,LibStub.lua,SharedMedia.lua} build/$i/\!GlobalFont_$e$w/
+		cp $1/\!GlobalFont_Template/{Core.lua,FixedSize.xml,LibStub.lua,SharedMedia.lua} build/$1/\!GlobalFont_$e$w/
 
-			cp font/$e/$ui-CL-$ext.otf build/$i/\!GlobalFont_$e$w/
-			cp font/$e/$ui-CL-$cond.otf build/$i/\!GlobalFont_$e$w/
-			cp font/$e/$sans-CN-$normal.otf build/$i/\!GlobalFont_$e$w/
-			cp font/$e/$sans-CN-$cond.otf build/$i/\!GlobalFont_$e$w/
-			cp font/$e/$sans-TW-$normal.otf build/$i/\!GlobalFont_$e$w/
-			cp font/$e/$sans-TW-$cond.otf build/$i/\!GlobalFont_$e$w/
+		cp font/$e/$ui-CL-$ext.otf build/$1/\!GlobalFont_$e$w/
+		cp font/$e/$ui-CL-$cond.otf build/$1/\!GlobalFont_$e$w/
+		cp font/$e/$sans-CN-$normal.otf build/$1/\!GlobalFont_$e$w/
+		cp font/$e/$sans-CN-$cond.otf build/$1/\!GlobalFont_$e$w/
+		cp font/$e/$sans-TW-$normal.otf build/$1/\!GlobalFont_$e$w/
+		cp font/$e/$sans-TW-$cond.otf build/$1/\!GlobalFont_$e$w/
 
-			# escaped twice by bash and sed
-			sed -i "s/__REPLACE_IN_BUILD__EDITION_ID__/$e$w/" build/$i/\!GlobalFont_$e$w/FixedSize.xml
-			sed -i "s/__REPLACE_IN_BUILD__DEFAULT_FONT_WESTERN__/Interface\\\\AddOns\\\\!GlobalFont_$e$w\\\\$ui-CL-$ext.otf/" build/$i/\!GlobalFont_$e$w/Core.lua build/$i/\!GlobalFont_$e$w/FixedSize.xml
-			sed -i "s/__REPLACE_IN_BUILD__CHAT_FONT_WESTERN__/Interface\\\\AddOns\\\\!GlobalFont_$e$w\\\\$ui-CL-$cond.otf/" build/$i/\!GlobalFont_$e$w/Core.lua
-			sed -i "s/__REPLACE_IN_BUILD__DEFAULT_FONT_ZHCN__/Interface\\\\AddOns\\\\!GlobalFont_$e$w\\\\$sans-CN-$normal.otf/" build/$i/\!GlobalFont_$e$w/Core.lua build/$i/\!GlobalFont_$e$w/FixedSize.xml
-			sed -i "s/__REPLACE_IN_BUILD__CHAT_FONT_ZHCN__/Interface\\\\AddOns\\\\!GlobalFont_$e$w\\\\$sans-CN-$cond.otf/" build/$i/\!GlobalFont_$e$w/Core.lua
-			sed -i "s/__REPLACE_IN_BUILD__DEFAULT_FONT_ZHTW__/Interface\\\\AddOns\\\\!GlobalFont_$e$w\\\\$sans-TW-$normal.otf/" build/$i/\!GlobalFont_$e$w/Core.lua build/$i/\!GlobalFont_$e$w/FixedSize.xml
-			sed -i "s/__REPLACE_IN_BUILD__CHAT_FONT_ZHTW__/Interface\\\\AddOns\\\\!GlobalFont_$e$w\\\\$sans-TW-$cond.otf/" build/$i/\!GlobalFont_$e$w/Core.lua
+		# escaped twice by bash and sed
+		sed -i "s/__REPLACE_IN_BUILD__EDITION_ID__/$e$w/" build/$1/\!GlobalFont_$e$w/FixedSize.xml
+		sed -i "s/__REPLACE_IN_BUILD__DEFAULT_FONT_WESTERN__/Interface\\\\AddOns\\\\!GlobalFont_$e$w\\\\$ui-CL-$ext.otf/" build/$1/\!GlobalFont_$e$w/Core.lua build/$1/\!GlobalFont_$e$w/FixedSize.xml
+		sed -i "s/__REPLACE_IN_BUILD__CHAT_FONT_WESTERN__/Interface\\\\AddOns\\\\!GlobalFont_$e$w\\\\$ui-CL-$cond.otf/" build/$1/\!GlobalFont_$e$w/Core.lua
+		sed -i "s/__REPLACE_IN_BUILD__DEFAULT_FONT_ZHCN__/Interface\\\\AddOns\\\\!GlobalFont_$e$w\\\\$sans-CN-$normal.otf/" build/$1/\!GlobalFont_$e$w/Core.lua build/$1/\!GlobalFont_$e$w/FixedSize.xml
+		sed -i "s/__REPLACE_IN_BUILD__CHAT_FONT_ZHCN__/Interface\\\\AddOns\\\\!GlobalFont_$e$w\\\\$sans-CN-$cond.otf/" build/$1/\!GlobalFont_$e$w/Core.lua
+		sed -i "s/__REPLACE_IN_BUILD__DEFAULT_FONT_ZHTW__/Interface\\\\AddOns\\\\!GlobalFont_$e$w\\\\$sans-TW-$normal.otf/" build/$1/\!GlobalFont_$e$w/Core.lua build/$1/\!GlobalFont_$e$w/FixedSize.xml
+		sed -i "s/__REPLACE_IN_BUILD__CHAT_FONT_ZHTW__/Interface\\\\AddOns\\\\!GlobalFont_$e$w\\\\$sans-TW-$cond.otf/" build/$1/\!GlobalFont_$e$w/Core.lua
 
-			cd build/$i
-			7z a -t7z -m0=LZMA:d=32m:fb=273 -ms=on ../../out/7z/GlobalFont_$e${w}_$v.7z \!GlobalFont_$e$w &
-			7z a -tzip -mm=Deflate:fb=258:pass=3 -mcu=on ../../out/zip/GlobalFont_$e${w}_$v.zip \!GlobalFont_$e$w &
-			cd ../..
-		done
+		cd build/$1
+		7z a -t7z -m0=LZMA:d=32m:fb=273 -ms=on ../../out/7z/GlobalFont_$e${w}_$v.7z \!GlobalFont_$e$w &
+		7z a -tzip -mm=Deflate:fb=258:pass=3 -mcu=on ../../out/zip/GlobalFont_$e${w}_$v.zip \!GlobalFont_$e$w &
+		cd ../..
 	done
 done
